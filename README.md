6e9732e251c63ce304e7d6fe42305d26bbfd6726
justgoscha <justgoscha@googlemail.com>
2014-01-31 16:28:44 +0100

    updating gh pages demo site

---
+++ README.md
@@ -0,0 +1,98 @@
+allmighty-autocomplete
+======================
+
+Simple to use autocomplete directive in a module for AngularJS!
+Supports arrow keys to traverse suggestions as well as mouse input. 
+You can load the suggestions from a remote REST API, it also supports promises. 
+
+Checkout [the demo](http://justgoscha.github.io/allmighty-autocomplete/) to see what it does.
+
+## Setup
+
+To use it you need jQuery and of course AngularJS, so make sure these are loaded first. I always like to use Google's CDN for that:
+
+```html
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.1.5/angular.min.js"></script>
+```
+
+Also you should load the stylesheet of the autocomplete:
+
+```html
+  <link rel="stylesheet" href="style/autocomplete.css">
+```
+
+Then in your HTML you should load it before the script of your main app. Like this:
+
+```html
+<script type="text/javascript" src="script/autocomplete.js"></script>
+<script type="text/javascript" src="script/app.js"></script>
+```
+
+In your main script file you should add it as dependency:
+
+```javascript
+var app = angular.module('app', ['autocomplete']);
+```
+
+## Usage
+
+If you now want an autocomplete you can just use the tag `<autocomplete>` tag in your HTML. With the `data` parameter you can pass in an array that will be used for autocompletion. You need to pass there something which is available in the $scope of your controller. 
+
+You can also pass a function that receives changes with the `on-type` attribute. This is useful if you need to load external resources from a REST API, for example, you cna then upload the array you passed into `data` and it will automatically use the changed array.
+
+### Attributes
+
+`data` : Pass an array to the autocomplete directive. Should be accessible in the $scope of your controller.
+
+`on-type` : (optional) Pass a function that will receives changes, when somebody types something. It passes the full string for any character typed or deleted. You can use that for example to update the array that you passed in data.
+
+`click-activation` : (optional) When `true`, the suggestion box opens on click (unfortunately onfoucs is not implemented properly in most browsers right now). By default it is only activated, when you start typing something.
+
+`placeholder`: (optional) Sets desired text as placeholder into the input element of autocomplete directive. By default it's "start typing..."
+
+## Example
+
+HTML: 
+```html
+    <div ng-controller="MyCtrl">  
+      <autocomplete data="movies" on-type="updateMovies"></autocomplete>
+    </div>
+```
+
+JavaScript:
+```javascript
+	var app = angular.module('app', ['autocomplete']);
+
+	app.controller('MyCtrl', function($scope, MovieRetriever){
+		$scope.movies = ["Lord of the Rings",
+		 				"Drive",
+		 				"Science of Sleep",
+		 				"Back to the Future",
+		 				"Oldboy"];
+
+		// gives another movie array on change
+		$scope.updateMovies = function(typed){
+			// MovieRetriever could be some service returning a promise
+		    $scope.newmovies = MovieRetriever.getmovies(typed);
+		    $scope.newmovies.then(function(data){
+		      $scope.movies = data;
+		    });
+		}
+	});
+
+```
+
+## Change log
+
+**31.01.2014**:
+
++ Stop showing suggestions on pushing escape
++ select if suggestions should be shown after clicking on input with new parameter `click-activation`
+
++ Added customizable placeholders for input line
+
++ FIXED multiple autocomplete directives in one controller are now possible and behave as expected
+
+
+
---
+++ script/autocomplete.js
@@ -0,0 +1,224 @@
+/* --- Made by justgoscha and licensed under MIT license --- */
+
+var app = angular.module('autocomplete', []);
+
+app.directive('autocomplete', function(){
+  var index = -1;
+
+  return {
+    restrict: 'E',
+    scope: {
+      suggestions: '=data',
+      onType: '=onType'
+    },
+    controller: function($scope, $element, $attrs){
+
+      $scope.searchParam;
+
+      // with the searchFilter the suggestions get filtered
+      $scope.searchFilter;
+
+      // the index of the suggestions that's currently selected
+      $scope.selectedIndex = -1;
+
+      // set new index
+      $scope.setIndex = function(i){
+        $scope.selectedIndex = parseInt(i);
+      }
+
+      this.setIndex = function(i){
+        $scope.setIndex(i);
+        $scope.$apply();
+      }
+
+      $scope.getIndex = function(i){
+        return $scope.selectedIndex;
+      }
+
+      // watches if the parameter filter should be changed
+      var watching = true;
+
+      // autocompleting drop down on/off
+      $scope.completing = false;
+
+      // starts autocompleting on typing in something
+      $scope.$watch('searchParam', function(){
+        if(watching && $scope.searchParam) {
+          $scope.completing = true;
+          $scope.searchFilter = $scope.searchParam;
+          $scope.selectedIndex = -1;
+        }
+
+        // function thats passed to on-type attribute gets executed
+        if($scope.onType)
+          $scope.onType($scope.searchParam);
+      });
+
+      // for hovering over suggestions
+      this.preSelect = function(suggestion){
+
+        watching = false;
+
+        // this line determines if it is shown 
+        // in the input field before it's selected:
+        //$scope.searchParam = suggestion;
+
+        $scope.$apply();
+        watching = true;
+
+      }
+
+      $scope.preSelect = this.preSelect;
+
+      this.preSelectOff = function(){
+        watching = true;
+      }
+
+      $scope.preSelectOff = this.preSelectOff;
+
+      // selecting a suggestion with RIGHT ARROW or ENTER
+      $scope.select = function(suggestion){
+        if(suggestion){
+          $scope.searchParam = suggestion;
+          $scope.searchFilter = suggestion;
+        }
+        watching = false;
+        $scope.completing = false;
+        setTimeout(function(){watching = true;},1000);
+        $scope.setIndex(-1);
+
+      }
+
+
+    },
+    link: function(scope, element, attrs){
+
+
+      scope.placeholder=attrs["placeholder"];
+      if(scope.placeholder===null||scope.placeholder===undefined)
+        scope.placeholder = "start typing..."
+      if(attrs["clickActivation"]=="true"){
+        element[0].onclick = function(e){
+          if(!scope.searchParam){
+            scope.completing = true;
+            scope.$apply();
+          }
+        };
+      }
+
+      var key = {left: 37, up: 38, right: 39, down: 40 , enter: 13, esc: 27};
+
+      document.addEventListener("keydown", function(e){
+        var keycode = e.keyCode || e.which;
+
+        switch (keycode){
+          case key.esc:
+            // disable suggestions on escape
+            scope.select();
+            scope.setIndex(-1);
+            scope.$apply();
+            e.preventDefault();
+        }
+      }, true);
+
+      element.keydown(function (e){
+        var keycode = e.keyCode || e.which;
+
+        var l = angular.element(this).find('li').length;
+
+        // implementation of the up and down movement in the list of suggestions
+        switch (keycode){
+          case key.up:    
+ 
+            index = scope.getIndex()-1;
+            if(index<-1){
+              index = l-1;
+            } else if (index >= l ){
+              index = -1;
+              scope.setIndex(index);
+              scope.preSelectOff();
+              break;
+            }
+            scope.setIndex(index);
+
+            if(index!==-1)
+            scope.preSelect(angular.element(this).find('li')[index].innerText);
+
+            scope.$apply();
+
+            break;
+          case key.down:
+            index = scope.getIndex()+1;
+            if(index<-1){
+              index = l-1;
+            } else if (index >= l ){
+              index = -1;
+              scope.setIndex(index);
+              scope.preSelectOff();
+              scope.$apply();
+              break;
+            }
+            scope.setIndex(index);
+            
+            if(index!==-1)
+            scope.preSelect(angular.element(this).find('li')[index].innerText);
+
+            break;
+          case key.left:    
+            break;
+          case key.right:  
+          case key.enter:  
+
+            index = scope.getIndex();
+            // scope.preSelectOff();
+            if(index !== -1)
+              scope.select(angular.element(this).find('li')[index].innerText);
+            scope.setIndex(-1);     
+            scope.$apply();
+
+            break;
+          case key.esc:
+            // disable suggestions on escape
+            scope.select();
+            scope.setIndex(-1);
+            scope.$apply();
+            e.preventDefault();
+            break;
+          default:
+            return;
+        }
+
+        if(scope.getIndex()!==-1 || keycode == key.enter)
+          e.preventDefault();
+      });
+    },
+    template: '<div class="autocomplete">'+
+                '<input type="text" ng-model="searchParam" placeholder="{{placeholder}}" />' +
+                '<ul ng-show="completing">' +
+                  '<li suggestion ng-repeat="suggestion in suggestions | filter:searchFilter | orderBy:\'toString()\'" '+
+                  'index="{{$index}}" val="{{suggestion}}" ng-class="{active: '+
+                  '($index == selectedIndex)}" ng-click="select(suggestion)">'+
+                    '{{suggestion}}'+
+                  '</li>'+
+                '</ul>'+
+              '</div>'
+    // templateUrl: 'script/ac_template.html'
+  }
+});
+
+app.directive('suggestion', function(){
+  return {
+    restrict: 'A',
+    require: '^autocomplete', // ^look for controller on parents element
+    link: function(scope, element, attrs, autoCtrl){
+      element.bind('mouseenter', function() {
+        autoCtrl.preSelect(attrs['val']);
+        autoCtrl.setIndex(attrs['index']);
+      });
+
+      element.bind('mouseleave', function() {
+        autoCtrl.preSelectOff();
+      });
+    }
+  }
+});
