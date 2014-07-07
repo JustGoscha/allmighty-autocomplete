a8e897c531445b978494f008561006feb863d0d4
David <david.mejorado@gmail.com>
2014-07-06 19:04:59 -0500

    moving template back into script - fixes #38

--- script/autocomplete.js
+++ script/autocomplete.js
@@ -219,7 +219,25 @@ app.directive('autocomplete', function() {
           e.preventDefault();
       });
     },
-    templateUrl: 'script/ac_template.html'
+    template: '\
+        <div class="autocomplete {{ attrs.class }}" id="{{ attrs.id }}">\
+          <input\
+            type="text"\
+            ng-model="searchParam"\
+            placeholder="{{ attrs.placeholder }}"\
+            class="{{ attrs.inputclass }}"\
+            id="{{ attrs.inputid }}"/>\
+          <ul ng-show="completing">\
+            <li\
+              suggestion\
+              ng-repeat="suggestion in suggestions | filter:searchFilter | orderBy:\'toString()\' track by $index"\
+              index="{{ $index }}"\
+              val="{{ suggestion }}"\
+              ng-class="{ active: ($index === selectedIndex) }"\
+              ng-click="select(suggestion)"\
+              ng-bind-html="suggestion | highlight:searchParam"></li>\
+          </ul>\
+        </div>'
   };
 });
 
