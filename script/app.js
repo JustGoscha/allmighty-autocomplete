8d80a5b45b4cddec9245dab8eab4634a93dee574
Art <maeto@hotmail.com>
2015-02-17 19:02:38 +0700

    Add example for grouping dropdown support

--- script/app.js
+++ script/app.js
@@ -1,53 +1,93 @@
-var app = angular.module('app', ['autocomplete']);
+var app = angular.module('app', ['autocomplete', 'angular.filter']);
 
 // the service that retrieves some movie title from an url
-app.factory('MovieRetriever', function($http, $q, $timeout){
-  var MovieRetriever = new Object();
+app.factory('MovieRetriever', function ($http, $q, $timeout) {
+    var MovieRetriever = new Object();
 
-  MovieRetriever.getmovies = function(i) {
-    var moviedata = $q.defer();
-    var movies;
+    MovieRetriever.getmovies = function (i) {
+        var moviedata = $q.defer();
 
-    var someMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel"];
+        var movies = [
+            'Die Hard',
+            'Terminator',
+            'Star Wars',
+            'Star Trek',
+            'Harry Potter',
+            'Narnia'
+        ];
 
-    var moreMovies = ["The Wolverine", "The Smurfs 2", "The Mortal Instruments: City of Bones", "Drinking Buddies", "All the Boys Love Mandy Lane", "The Act Of Killing", "Red 2", "Jobs", "Getaway", "Red Obsession", "2 Guns", "The World's End", "Planes", "Paranoia", "The To Do List", "Man of Steel", "The Way Way Back", "Before Midnight", "Only God Forgives", "I Give It a Year", "The Heat", "Pacific Rim", "Pacific Rim", "Kevin Hart: Let Me Explain", "A Hijacking", "Maniac", "After Earth", "The Purge", "Much Ado About Nothing", "Europa Report", "Stuck in Love", "We Steal Secrets: The Story Of Wikileaks", "The Croods", "This Is the End", "The Frozen Ground", "Turbo", "Blackfish", "Frances Ha", "Prince Avalanche", "The Attack", "Grown Ups 2", "White House Down", "Lovelace", "Girl Most Likely", "Parkland", "Passion", "Monsters University", "R.I.P.D.", "Byzantium", "The Conjuring", "The Internship"]
+        $timeout(function () {
+            moviedata.resolve(movies);
+        }, 1000);
 
-    if(i && i.indexOf('T')!=-1)
-      movies=moreMovies;
-    else
-      movies=moreMovies;
+        return moviedata.promise
+    };
 
-    $timeout(function(){
-      moviedata.resolve(movies);
-    },1000);
-
-    return moviedata.promise
-  }
-
-  return MovieRetriever;
+    return MovieRetriever;
 });
 
-app.controller('MyCtrl', function($scope, MovieRetriever){
+app.factory('MovieRetriever2', function ($http, $q, $timeout) {
+    var MovieRetriever2 = new Object();
 
-  $scope.movies = MovieRetriever.getmovies("...");
-  $scope.movies.then(function(data){
-    $scope.movies = data;
-  });
+    MovieRetriever2.getmovies = function (i) {
+        var moviedata = $q.defer();
 
-  $scope.getmovies = function(){
-    return $scope.movies;
-  }
+        var movies = [
+            {value: 'Die Hard', group: 'Action'},
+            {value: 'Terminator', group: 'Action'},
+            {value: 'Star Wars', group: 'Sci-Fi'},
+            {value: 'Star Trek', group: 'Sci-Fi'},
+            {value: 'Harry Potter', group: 'Kids'},
+            {value: 'Narnia', group: 'Kids'}
+        ];
 
-  $scope.doSomething = function(typedthings){
-    console.log("Do something like reload data with this: " + typedthings );
-    $scope.newmovies = MovieRetriever.getmovies(typedthings);
-    $scope.newmovies.then(function(data){
-      $scope.movies = data;
+        $timeout(function () {
+            moviedata.resolve(movies);
+        }, 1000);
+
+        return moviedata.promise
+    };
+
+    return MovieRetriever2;
+});
+
+app.controller('MyCtrl', function ($scope, MovieRetriever, MovieRetriever2) {
+
+    $scope.movies = MovieRetriever.getmovies("...");
+    $scope.moviesByGenre = MovieRetriever2.getmovies("...");
+    $scope.movies.then(function (data) {
+        $scope.movies = data;
     });
-  }
 
-  $scope.doSomethingElse = function(suggestion){
-    console.log("Suggestion selected: " + suggestion );
-  }
+    $scope.getmovies = function () {
+        return $scope.movies;
+    };
 
+    $scope.doSomething = function (typedthings) {
+        console.log("Do something like reload data with this: " + typedthings);
+        $scope.newmovies = MovieRetriever.getmovies(typedthings);
+        $scope.newmovies.then(function (data) {
+            $scope.movies = data;
+        });
+    };
+
+    $scope.doSomethingElse = function (suggestion) {
+        console.log("Suggestion selected: " + suggestion);
+    };
+
+    $scope.getmovies2 = function(){
+        return $scope.moviesByGenre;
+    };
+
+    $scope.doSomething2 = function(typedthings){
+        console.log("Do something like reload data with this: " + typedthings);
+        $scope.newmovies = MovieRetriever2.getmovies(typedthings);
+        $scope.newmovies.then(function (data) {
+            $scope.moviesByGenre = data;
+        });
+    };
+
+    $scope.doSomethingElse2 = function (suggestion) {
+        console.log("Suggestion selected: " + suggestion);
+    };
 });
